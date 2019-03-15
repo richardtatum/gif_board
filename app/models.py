fc36eeb788e2f16b64061c8df1655f648ad739a1
Richard Tatum <rtatum@pm.me>
2019-03-15 15:43:28 +0000

    Flask App framework

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+from app import login
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    r6_user = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    gifs = db.relationship('Gif', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.r6_user}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, salt_length=32)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Gif(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    frame = db.Column(db.String(256))
+    mp4 = db.Column(db.String(256))
+    webm = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Gif {self.id} for: {self.author}.>'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
---
+++ app/routes.py
@@ -0,0 +1,49 @@
+from flask import render_template, flash, redirect, url_for, request, session, abort
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Gif
+from datetime import datetime
+
+
+@app.route('/')
+@login_required
+def home():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    gifs = Gif.query.all()
+    return render_template('home.html', gifs=gifs)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data.lower()).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password', 'danger')
+            return redirect(url_for('login'))
+        login_user(user)
+        app.logger.info(f'{user} logged in.')
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been successfully logged out.', 'success')
+    return redirect(url_for('login'))
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
